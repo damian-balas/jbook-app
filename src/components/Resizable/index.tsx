f4f22e77898f27f8aef2376fa0016352b638c7de
Damian Balas <damian.webdev@gmail.com>
2021-03-03 20:07:17 +0100

    fix stuck size in Resizable component on window resize

--- src/components/Resizable/index.tsx
+++ src/components/Resizable/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
+import debounce from "../../utils/debounce";
 import "./Resizable.scss";
 
 interface ResizableProps {
@@ -7,36 +9,50 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(innerWidth * 0.75);
+
+  useEffect(() => {
+    const listener = () => {
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
+      if (window.innerWidth * 0.75 < width) {
+        setWidth(window.innerWidth * 0.75);
+      }
+    };
+    const debouncedListener = debounce(listener, 100);
+
+    window.addEventListener("resize", debouncedListener);
+
+    return () => {
+      window.removeEventListener("resize", debouncedListener);
+    };
+  }, [width]);
 
   if (direction === "horizontal") {
     resizableProps = {
       className: "resize-horizontal",
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width,
       resizeHandles: ["e"],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
       minConstraints: [Infinity, 40],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       height: 300,
       width: Infinity,
       resizeHandles: ["s"],
     };
   }
 
-  return (
-    <ResizableBox
-      {...resizableProps}
-      onResize={() => {
-        console.log(document.querySelector("iframe")?.getBoundingClientRect());
-      }}
-    >
-      {children}
-    </ResizableBox>
-  );
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
