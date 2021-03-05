e7981e2905a14c9d02d7376dc3a38a3148c243c4
Damian Balas <damian.webdev@gmail.com>
2021-03-05 23:36:46 +0100

    show TextEditor and temporarily hide CodeCell

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom";
 
 import CodeCell from "./components/CodeCell";
+import TextEditor from "./components/TextEditor";
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 };
