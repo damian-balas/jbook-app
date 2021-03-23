f925821f3d05289d70f6757b0004f9ee18408174
Damian Balas <damian.webdev@gmail.com>
2021-03-23 21:00:22 +0100

    change serve log

--- packages/cli/src/loggers/serve.ts
+++ packages/cli/src/loggers/serve.ts
@@ -4,11 +4,10 @@ import { getLocalIp } from "../utils/getLocalIp";
 export function logInfoAfterServe(filename: string, port: string) {
   console.log(
     chalk`
-      Opened {green.bold ${filename}}. Navigate to:
+{yellow.bold ${filename}} - To edit the file, please navigate to:
 
-      {green.bold http://localhost:${port}} or {green.bold http://${getLocalIp()}:${port}}
-      
-      to edit the file.
+  {green.bold http://localhost:${port}} 
+  {green.bold http://${getLocalIp()}:${port}}
     `,
   );
 }
